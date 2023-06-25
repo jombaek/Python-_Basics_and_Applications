df8740014b835048ad9bab075c641713f6cab791
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2023-06-25 16:02:39 +0700

    Class MoneyBox

---
+++ 1.5.1.py
@@ -0,0 +1,10 @@
+class MoneyBox:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.coins = 0
+
+    def can_add(self, v):
+        return self.coins + v <= self.capacity
+
+    def add(self, v):
+        self.coins += v
